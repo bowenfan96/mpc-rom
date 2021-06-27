0c1ac889a35e8bfabdd1efbf52f4e8839443e755
bowen <bowen@users.noreply.github.com>
2021-06-28 04:32:22 +0800

    update system

--- mpc.py
+++ mpc.py
@@ -15,12 +15,13 @@ import pandas as pd
 model = ConcreteModel()
 # Define the set of continuous time
 model.time = ContinuousSet(bounds=(0, 100))
+assert model.time.last() == 100
 
 # Define system variables
-model.x1 = Var(model.time, initialize=0)
+model.x1 = Var(model.time, initialize=10)
 model.x2 = Var(model.time, initialize=0)
 model.x3 = Var(model.time, initialize=0)
-model.u = Var(model.time, initialize=1)
+model.u = Var(model.time, initialize=0)
 
 # Define derivative variables
 model.dx1dt = DerivativeVar(model.x1, wrt=model.time, initialize=0)
@@ -28,7 +29,7 @@ model.dx2dt = DerivativeVar(model.x2, wrt=model.time, initialize=0)
 model.dx3dt = DerivativeVar(model.x3, wrt=model.time, initialize=0)
 
 discretizer = TransformationFactory('dae.finite_difference')
-discretizer.apply_to(model, nfe=100, wrt=model.time, scheme='BACKWARD')
+discretizer.apply_to(model, nfe=model.time.last(), wrt=model.time, scheme='BACKWARD')
 
 # ODEs and constraints
 # Flawed model used in MPC
@@ -41,51 +42,52 @@ discretizer.apply_to(model, nfe=100, wrt=model.time, scheme='BACKWARD')
 # dx2dt = x1 - x2
 # dx3dt = -x3 + 0.3x1^2
 
-def _setPoint(m, t):
-    # if t > 20:
-    #     return m.x1[t] == 0.02*t**2 - 0.5*t
-    if t == 100:
-        return m.x1[t] == 10
-    else:
-        return Constraint.Skip
-model.x1[0].fix(0)
+# def _setPoint(m, t):
+#     # if t > 20:
+#     #     return m.x1[t] == 0.02*t**2 - 0.5*t
+#     if t == 100:
+#         return m.x1[t] == 10
+#     else:
+#         return Constraint.Skip
+model.x1[0].fix(10)
 model.x2[0].fix(0)
 model.x3[0].fix(0)
+model.u[0].fix(0)
 
 def _ode1(m, t):
     # For testing to check that the control is working (next line only)
     # return m.dx1dt[t] == 0.05 * m.u[t]
 
-    return m.dx1dt[t] == -1.5*m.x1[t] + 0.9*m.x2[t] + m.x3[t] + 1.2*m.u[t]
+    return m.dx1dt[t] == 2*m.x1[t] + m.x2[t] + m.x3[t] + m.u[t]
 
 def _ode2(m, t):
-    return m.dx2dt[t] == 1.2*m.x1[t] - 0.8*m.x2[t]
+    return m.dx2dt[t] == m.x1[t] - m.x2[t]
 
 def _ode3(m, t):
-    return m.dx3dt[t] == -0.8*m.x3[t] + 0.2*m.x1[t]**2
+    return m.dx3dt[t] == -m.x3[t] - 0.2*m.x1[t]**2
 
-model.c1 = Constraint(model.time, rule=_setPoint)
+# model.c1 = Constraint(model.time, rule=_setPoint)
 model.c2 = Constraint(model.time, rule=_ode1)
 model.c3 = Constraint(model.time, rule=_ode2)
 model.c4 = Constraint(model.time, rule=_ode3)
 
 # Controller constraint
-# def _u_constraint(m, t):
-#     return abs(m.u[t]) <= 500
-# model.c5 = Constraint(model.time, rule=_u_constraint)
+def _u_constraint(m, t):
+    return abs(m.u[t]) <= 0.5
+model.c5 = Constraint(model.time, rule=_u_constraint)
 
 # Define objective function
 # Objective: Least control cost
-model.obj = Objective(
-    expr=sum((0.5 * model.u[t] ** 2) for t in model.time),
-    sense=minimize
-)
-
-# Objective: Least error from setpoint
 # model.obj = Objective(
-#     expr=sum((model.x1[t] - 100)**2 for t in model.time),
+#     expr=sum((0.5 * model.u[t] ** 2) for t in model.time),
 #     sense=minimize
 # )
+
+# Objective: Least error from setpoint
+model.obj = Objective(
+    expr=sum((model.x1[int(t)] - 0)**2 for t in model.time),
+    sense=minimize
+)
 
 opt = SolverFactory('ipopt', tee=True)
 opt.options['max_iter'] = 1000
@@ -115,23 +117,29 @@ for time in model.time:
     mpc_x3_plot.append(value(model.x3[time]))
     mpc_u_plot.append(value(model.u[time]))
 
-    sys_x1_plot.append(new_state[0])
-    sys_x2_plot.append(new_state[1])
-    sys_x3_plot.append(new_state[2])
+    if time == 0:
+        sys_x1_plot.append(value(model.x1[time]))
+        sys_x2_plot.append(value(model.x2[time]))
+        sys_x3_plot.append(value(model.x3[time]))
 
-    # Fix new state
-    step = 1
-    if time < 100:
-        model.x1[time + step].fix(new_state[0])
-        model.x2[time + step].fix(new_state[1])
-        model.x3[time + step].fix(new_state[2])
+    elif time < len(model.time):
+        sys_x1_plot.append(new_state[0])
+        sys_x2_plot.append(new_state[1])
+        sys_x3_plot.append(new_state[2])
+
+    # Record system and controller state and fix new state from simulated system
+    # step = 1
+    # if time < len(model.time):
+    #     model.x1[time + step].fix(new_state[0])
+    #     model.x2[time + step].fix(new_state[1])
+    #     model.x3[time + step].fix(new_state[2])
 
     print('Time: ', time)
     print("MPC")
     print("x1: ", value(model.x1[time]), "\tx2: ", value(model.x2[time]),
           "\tx3: ", value(model.x3[time]), "\tu: ", value(model.u[time]))
     print("System")
-    print("x1: ", new_state[0], "\tx2: ", new_state[1], "\tx3", new_state[2])
+    print("x1: ", sys_x1_plot[int(time)], "\tx2: ", sys_x2_plot[int(time)], "\tx3", sys_x3_plot[int(time)])
 
 plt.plot(model.time, mpc_x1_plot, label='mpc_x1')
 plt.plot(model.time, mpc_x2_plot, label='mpc_x2')
--- system.py
+++ system.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.integrate
 
+import matplotlib.pyplot as plt
+
 # dx1dt = -2x1 + x2 + x3 + u
 # dx2dt = x1 - x2
 # dx3dt = x1 - x3 - x1^2
@@ -14,9 +16,9 @@ def step_system(current_state, time, controls, step_len=1):
         x2 = state[1]
         x3 = state[2]
 
-        dx1dt = -2*x1 + x2 + x3 + u
+        dx1dt = 2*x1 + x2 + x3 + u
         dx2dt = x1 - x2
-        dx3dt = -x3 + 0.25*x1**2
+        dx3dt = -x3 - 0.2*x1**2
 
         return [dx1dt, dx2dt, dx3dt]
 
@@ -25,3 +27,36 @@ def step_system(current_state, time, controls, step_len=1):
     system.set_initial_value(current_state, time)
     new_state = np.array(system.integrate(system.t + step_len))
     return new_state
+
+
+def simulate_and_get_data(duration):
+    x1_i = 10
+    x2_i = 0
+    x3_i = 0
+
+    current_state = [x1_i, x2_i, x3_i]
+    controls = [0]
+
+    sys_x1_plot = []
+    sys_x2_plot = []
+    sys_x3_plot = []
+
+    for time in range(duration):
+        sys_x1_plot.append(current_state[0])
+        sys_x2_plot.append(current_state[1])
+        sys_x3_plot.append(current_state[2])
+
+        current_state = step_system(current_state, duration, controls, step_len=1)
+
+    plt.plot(range(duration), sys_x1_plot, label='sys_x1')
+    plt.plot(range(duration), sys_x2_plot, label='sys_x2')
+    plt.plot(range(duration), sys_x3_plot, label='sys_x3')
+
+    plt.xlabel("Time")
+    plt.legend()
+    plt.savefig("plot.svg", format="svg")
+    plt.show()
+
+
+if __name__ == "__main__":
+    simulate_and_get_data(100)
