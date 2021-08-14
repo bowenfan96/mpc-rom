7da1701aca6e7f2d6b583a253f2d9a531b939357
bowen <bowen@users.noreply.github.com>
2021-08-15 04:51:48 +0800

    organize files

--- archive/main.py
+++
@@ -1,9 +0,0 @@
-# For integrating the system model (system.py), MPC controller (mpc.py),
-# and the model reduction tool (autoencoder.py and possibly other methods)
-
-import system
-import mpc
-import autoencoder
-
-import control
-
