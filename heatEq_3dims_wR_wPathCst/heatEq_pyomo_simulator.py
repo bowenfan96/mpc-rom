b282e3b0849c7e0a94aad6fcd11a3b26e4263a67
bowen <bowen@users.noreply.github.com>
2021-08-17 07:11:56 +0800

    cd point07

--- heatEq_3dims_wR_wPathCst/heatEq_pyomo_simulator.py
+++ heatEq_3dims_wR_wPathCst/heatEq_pyomo_simulator.py
@@ -11,7 +11,7 @@ from pyomo.solvers import *
 
 from heatEq_nn_controller import *
 
-results_folder = "expReplay_results/sprite10/"
+results_folder = "expReplay_results/point07/"
 
 
 class HeatEqSimulator:
