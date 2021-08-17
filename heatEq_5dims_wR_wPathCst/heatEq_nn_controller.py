24ab5d4c87d75a4fceaadea8dc49a6d974049156
bowen <bowen@users.noreply.github.com>
2021-08-17 22:26:59 +0800

    5 dim with path cst cd texel13

--- heatEq_5dims_wR_wPathCst/heatEq_nn_controller.py
+++ heatEq_5dims_wR_wPathCst/heatEq_nn_controller.py
@@ -23,7 +23,7 @@ from scipy import optimize
 import time as python_timer
 
 data_folder = "data/"
-results_folder = "expReplay_results/arc12/"
+results_folder = "expReplay_results/texel13/"
 
 
 class xMOR(nn.Module):
