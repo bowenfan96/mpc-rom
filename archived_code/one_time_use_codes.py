86231ae4dce61ec63e95fbc8ef483bb5d293e929
bowen <bowen@users.noreply.github.com>
2021-08-21 02:25:55 +0800

    bpod with impulse 0.05s intervals gonna try 0.01s

--- archived_code/one_time_use_codes.py
+++ archived_code/one_time_use_codes.py
@@ -49,3 +49,55 @@
 #     cst_plot.append(pred_cst)
 #
 # print(python_timer.time() - start_time)
+
+
+# GENERATE MATRIX FOR MATLAB
+# ----- GENERATE THE MODEL MATRICES -----
+# Apply the method of lines on the heat equation to generate the A matrix
+# Length of the rod = 1 m
+# Number of segments = number of discretization points - 1 (as 2 ends take up 2 points)
+import numpy as np
+
+N = 20
+length = 1
+num_segments = N - 1
+# Thermal diffusivity alpha
+alpha = 0.3
+segment_length = length / num_segments
+# Constant
+c = alpha / (segment_length ** 2)
+
+# Generate A matrix
+A_mat = np.zeros(shape=(N, N))
+for row in range(A_mat.shape[0]):
+    for col in range(A_mat.shape[1]):
+        if row == col:
+            A_mat[row][col] = -2
+        elif abs(row - col) == 1:
+            A_mat[row][col] = 1
+        else:
+            A_mat[row][col] = 0
+# Multiply constant to all elements in A
+A = c * A_mat
+
+# Generate B matrix
+# Two sources of heat at each end of the rod
+num_heaters = 2
+B_mat = np.zeros(shape=(N, num_heaters))
+# First heater on the left
+B_mat[0][0] = 1
+# Second heater on the right
+B_mat[N - 1][num_heaters - 1] = 1
+# Multiply constant to all elements in B
+B = c * B_mat
+
+# Print A and B for matlab
+# for i in range(A.shape[0]):
+#     for j in range(A.shape[1]):
+#         print(round(A[i][j], 5), end=" ")
+#     print(";", end=" ")
+
+for i in range(B.shape[0]):
+    for j in range(B.shape[1]):
+        print(round(B[i][j], 5), end=" ")
+    print(";", end=" ")
